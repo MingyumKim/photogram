732fe8dd0a34c2dccbef195d90ea58f145ec61a3
mingyumkim <pocket5004@gmail.com>
2022-11-03 17:50:53 +0900

    - 로그인 처리

---
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,31 @@
+package com.cos.photogramstart.config.auth;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    //1.패스워드는 자동으로 체킹
+    //2.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User userEntity = userRepository.findByUsername(username);
+
+        if(userEntity == null) {
+            return null;
+        }else{
+            return new PrincipalDetails(userEntity);
+        }
+    }
+}
--- src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //어노테이션이 없어도 IoC 자동 등록된다
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //JPA query method
+    User findByUsername(String username);
 
 }
