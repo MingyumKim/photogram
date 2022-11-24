be49fde51cb27ea909fec7e2e80a8e1a113cd3be
mingyumkim <pocket5004@gmail.com>
2022-11-24 15:23:26 +0900

    - 세션정보 찾기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +15,14 @@ public class UserController {
         return "user/profile";
     }
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id){
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        //방법 1.
+        System.out.println("session : " + principalDetails.getUser());
+
+        //방법 2.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
+        System.out.println("session2 : " + mPrincipalDetails.getUser());
         return "user/update";
     }
 }
